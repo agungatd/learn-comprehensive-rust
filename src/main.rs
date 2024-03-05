f1c14b88827b988aa93220c9ffd41bfec6993434
agungatd <agungatidhira@gmail.com>
2024-03-05 11:07:40 +0700

    learn about arithmetic, and test overflow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    run_variables();
+    // run_variables();
+    run_arithmetic()
 }
 
 fn run_variables() {
@@ -20,9 +21,27 @@ fn run_variables() {
 
     // characters
     let grade: char = 'A';
-    println!("My grade is {grade}")
+    println!("My grade is {grade}");
 
     // Booleans
     let is_male: bool = true;
-    println!("My sex is {is_male}");
+    println!("My sex is Male == {is_male}");
+}
+
+fn circle_area(radius: f32) -> f32 {
+    let phi: f32 = 3.14;
+    return phi * radius * radius;
+}
+
+fn interproduct(a: i32, b: i32, c: i32) -> i32 {
+    return a * b + b * c + c * a;
+}
+
+fn run_arithmetic() {
+    // * example of function that calculate area of a circle
+    // let radius: f32 = 7.001;
+    // println!("The area of a circle with radius {radius} is {}", circle_area(radius));
+
+    // test overflow, change i32 to i16
+    println!("interproduct result: {}", interproduct(120, 100, 248));
 }
