f8ae7cb53a86b2b3a492883a027509605b3caa61
agungatd <agungatidhira@gmail.com>
2024-03-05 13:50:36 +0700

    add remote repo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    // run_variables();
-    // run_arithmetic();
+    run_variables();
+    run_arithmetic();
     run_strings();
 }
 
@@ -53,17 +53,18 @@ fn run_variables() {
 
 fn circle_area(radius: f32) -> f32 {
     let phi: f32 = 3.14;
-    return phi * radius * radius;
+    
+    phi * radius * radius
 }
 
 fn interproduct(a: i32, b: i32, c: i32) -> i32 {
-    return a * b + b * c + c * a;
+    a * b + b * c + c * a
 }
 
 fn run_arithmetic() {
-    // * example of function that calculate area of a circle
-    // let radius: f32 = 7.001;
-    // println!("The area of a circle with radius {radius} is {}", circle_area(radius));
+    // example of function that calculate area of a circle
+    let radius: f32 = 7.001;
+    println!("The area of a circle with radius {radius} is {}", circle_area(radius));
 
     // test overflow, change i32 to i16
     println!("interproduct result: {}", interproduct(120, 100, 248));
