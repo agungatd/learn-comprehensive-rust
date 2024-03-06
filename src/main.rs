720c8a06f3b638870fcb598f3c42ca66ef88e77e
agungatd <agungatidhira@gmail.com>
2024-03-06 14:13:09 +0700

    finish if conditional, loops, and break & continue

---
+++ src/main.rs
@@ -0,0 +1,75 @@
+fn age_cond(age: u32) -> String {
+    // the if conditional is similar with other languages
+    if age < 18 {
+        "You are too young!".to_string()
+    } else if age < 60 {
+        "work, work, work!".to_string()
+    } else {
+        "You are a senior!".to_string()
+    }
+}
+
+fn loops() {
+    let mut x = 200;
+    // while loop
+    while x >= 10 {
+        x = x / 2;
+    }
+    println!("x: {x}");
+
+    // for loop
+    for n in 1..=x {
+        println!("for n = {n}");
+    }
+
+    for elem in [1,2,3] {
+        println!("element no: {elem}");
+    }
+
+    // `loop` statment just loops forevah until a `break`. use when:
+    // - When you don't know in advance how many times the loop should run.
+    // - You need a loop that's guaranteed to run at least once.
+    // - You can use the break keyword for flexible termination based on various factors within the loop's body.
+    loop {
+        x += 1;
+        if x > 10 {
+            break;
+        }
+    }
+    println!("x after loop: {x}");
+}
+
+fn loops_break_label() {
+    let s = [
+        [[5, 6, 7], [8, 9, 10], [21, 15, 32]],
+        [[5, 6, 7], [8, 9, 77], [21, 15, 32]],
+        [[5, 6, 7], [8, 9, 10], [21, 15, 32]]
+    ];
+    let mut elements_searched = 0;
+    let target_value = 10;
+    'outer: for i in 0..=2 {
+        'inner2nd: for j in 0..=2 {
+            println!("inside inner2nd");
+            for k in 0..=2 {
+                elements_searched += 1;
+                if s[i][j][k] == target_value {
+                    println!("break");
+                    // break; if no label, will go to inner2nd loop
+                    break 'outer;
+                }
+            }
+        }
+    }
+    println!("elements searched: {elements_searched}");
+}
+
+fn main() {
+    // let age: u32 = 18;
+    // let x = age_cond(age);
+    // // if as an expression
+    // let y = if age < 18 {"Young!"} else {"Adult"};
+    // println!("the age category: {x}");
+    // println!("the age is: {y}");
+    // loops();
+    loops_break_label();
+}
